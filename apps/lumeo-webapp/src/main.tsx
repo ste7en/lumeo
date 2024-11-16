d1dde3aa391cb6cf9a5e9331e7afa1c70934a099
Stefano Formicola <stefanoformicola@icloud.com>
2024-11-17 00:28:00 +0100

    feat(webapp): add environment variable for API base URL and configure axios

--- apps/lumeo-webapp/src/main.tsx
+++ apps/lumeo-webapp/src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
+import axios from 'axios';
+import { baseURL } from './config';
+
+axios.defaults.baseURL = baseURL;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
