8c2ac49a859181c3fcc64bbf6173e086d269f927
Stefano Formicola <stefanoformicola@icloud.com>
2024-11-17 15:28:43 +0100

    feat(webapp): implement ImagePage and ImageDetails components with routing

--- apps/lumeo-webapp/src/App.tsx
+++ apps/lumeo-webapp/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Camera, Heart } from 'lucide-react';
 import { HomePage } from './pages/HomePage';
-// import { ImagePage } from './pages/ImagePage';
+import { ImagePage } from './pages/ImagePage';
 
 const queryClient = new QueryClient();
 
@@ -32,7 +32,7 @@ function App() {
 
           <Routes>
             <Route path="/" element={<HomePage />} />
-            {/* <Route path="/image/:id" element={<ImagePage />} /> */}
+            <Route path="/image/:id" element={<ImagePage />} />
           </Routes>
         </div>
       </BrowserRouter>
